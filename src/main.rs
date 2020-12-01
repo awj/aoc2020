fdcf8e387529606a6609a5cf572f49fe4250653d
Adam Jones <ajones1@gmail.com>
2020-11-30 22:58:55 -0700

    day1 star 1.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod day1;
+
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let file = &args[1];
+
+    if let Ok(contents) = fs::read_to_string(file) {
+        match day1::find_pair_with_total(contents.lines().map(|x| x.parse::<i32>().unwrap())) {
+            Some((x,y)) => println!("Result {}", x * y),
+            None => println!("No pairs")
+        }
+    }
+
 }
