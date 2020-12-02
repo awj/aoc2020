7a588172d1e94c726f2f06fe9a81a9eec35aa6d4
Adam Jones <ajones1@gmail.com>
2020-12-02 11:09:10 -0700

    Day 2, star 2.

--- src/day2.rs
+++ src/day2.rs
@@ -23,6 +23,20 @@ impl PWSpec {
     }
 
     pub fn valid(&self, password: &str) -> bool {
+        // min/max should "always" be a valid index into the password, so we can
+        // just panic! if we either cannot convert it to `usize` or we go out of
+        // bounds on the character list.
+        let first_index = usize::try_from(self.min - 1).unwrap();
+        let first_char = password.chars().nth(first_index).unwrap();
+
+        let second_index = usize::try_from(self.max - 1).unwrap();
+        let second_char = password.chars().nth(second_index).unwrap();
+
+        ( first_char == self.char && second_char != self.char ) ||
+            ( first_char != self.char && second_char == self.char )
+    }
+
+    pub fn old_valid(&self, password: &str) -> bool {
         let instances = password.matches(|c| c == self.char).count();
         let instance_count = i32::try_from(instances).unwrap();
 
@@ -36,6 +50,15 @@ pub struct PWLine<'a> {
     password: &'a str
 }
 
+// Note: we're saying that the string slice going into `PWLine` as the password
+// cannot outlive the input string we're parsing (since that's what it
+// references).
+//
+// Seems like maybe we could/should use a dependent lifetime (impl<'a: 'b, 'b>
+// ...) here for flexibility so that string slice can have an
+// independent-but-smaller lifetime than the input? Can't figure out how to get
+// that to compile, nor do I understand errors, so I guess this is good
+// enough...
 impl<'a> PWLine<'a> {
     pub fn parse(s: &'a str) -> Option<Self> {
         let parts: Vec<&str> = s.split(": ").collect();
@@ -45,7 +68,7 @@ impl<'a> PWLine<'a> {
         Some(
             PWLine {
                 spec: spec,
-                password: parts[1].clone()
+                password: parts[1]
             }
         )
     }
