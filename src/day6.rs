6d7a9e3c0db2d97c54bed4fe23b734879507de44
Adam Jones <ajones1@gmail.com>
2020-12-06 17:33:44 -0700

    Day 6 notes.

--- src/day6.rs
+++ src/day6.rs
@@ -27,17 +27,21 @@ pub fn all_yes<'a>(input: &'a mut std::str::Lines) -> Vec<usize> {
     let mut sizes: Vec<usize> = Vec::new();
     let mut fresh_group = true;
 
+    // I *really* need a better solution for this "group input by new line
+    // separators" thing. It's super annoying to have to keep checking edge
+    // cases. (IOW I didn't here after getting it right for the first star...)
     for line in input {
         if line.is_empty() {
             fresh_group = true;
             sizes.push(answers.len());
             answers.clear();
         } else {
-            let line_answers = get_answers(line);
             if fresh_group {
-                answers = line_answers;
+                answers = get_answers(line);
                 fresh_group = false;
             } else {
+                // Kinda weird that there's no reduce-to-intersection method for
+                // a HashSet. Instead we end up having to allocate new sets
                 answers = answers.intersection(&line_answers).map(|c| c.clone() ).collect();
             }
         }
